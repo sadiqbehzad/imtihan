c0be64ad99b95dde1940f33cfa168408be504a1c
lele515 <lemasafi15@gmail.com>
2024-02-02 14:37:29 -0700

    Added the navigation to the button component.

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import "./button.css";
-const Button = (props) => {
-  const { width, label, color } = props;
-  const btnStyle = {
-    width: width || "auto",
-    color: color || "White",
-  };
+import { useNavigate } from "react-router-dom";
 
-  return (
-    <div className="btn-container">
-      <button className="btn" style={btnStyle}>
-        {label}{" "}
-      </button>
-    </div>
-  );
-};
+
+  const Button = (props) => {
+    const { width, label, color, to } = props;
+    const navigate = useNavigate();
+
+    const btnStyle = {
+      width: width || "auto",
+      color: color || "White",
+    };
+
+    const handleClick = () => {
+      navigate(to);
+    };
+    return (
+      <div className="btn-container">
+        <button className="btn" style={btnStyle} onClick={handleClick}>
+          {label}{" "}
+        </button>
+      </div>
+    );
+  };
+  
 export default Button;
+
