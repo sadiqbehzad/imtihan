0ed48ed8ff5317fb27151988b974c6b242371051
lele515 <lemasafi15@gmail.com>
2024-02-03 13:26:19 -0700

    fixed the services error.

--- src/pages/owners/Owners.jsx
+++ src/pages/owners/Owners.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import OwnersBenefits from './OwnersBenefits/OwnersBenefits';
 import Footer from'../../components/footer/Footer';
-import OwnerServices from './OwnersServices /OwnersServices';
-import OwnersTestimony from './OwnersTestimony /OwnersTestimony';
+import OwnerServices from './OwnersServices/OwnersServices';
+import OwnersTestimony from './OwnersTestimony/OwnersTestimony';
 import OwnersProducts from './OwnersProducts/OwnersProducts';
 import OwnerHeader from './OwnersHeader/OwnersHeader';
 
