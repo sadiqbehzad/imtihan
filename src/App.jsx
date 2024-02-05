094782da144fe2d3cfafe84679bc6eccaf8274a3
lele515 <lemasafi15@gmail.com>
2024-02-05 11:48:09 -0700

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from '../../components/navbar/Navbar';
+import Navbar from './components/navbar/Navbar';
 import Carousel from "./components/carousel/Carousel";
 import Solutions from "./components/solutions/Solutions";
 import Blog from "./components/blog/Blog";
