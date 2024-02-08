9e0104ddcd418043538dc84e815e75edd8f04ce5
sadiqbehzad <147103374+sadiqbehzad@users.noreply.github.com>
2024-02-08 11:10:28 -0700

    checking

--- src/pages/owners/Owners.jsx
+++ src/pages/owners/Owners.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Navbar from '../../components/navbar/Navbar';
-import OwnersBenefits from './OwnersBenefits/OwnersBenefits';
-import Footer from'../../components/footer/Footer';
-import OwnersServices from './OwnersServices/OwnersServices';
-import OwnersTestimony from './OwnersTestimony/OwnersTestimony';
-import OwnersProducts from './OwnersProducts/OwnersProducts';
-import OwnerHeader from './OwnersHeader/OwnersHeader';
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import OwnersBenefits from "./OwnersBenefits/OwnersBenefits";
+import Footer from "../../components/footer/Footer";
+import OwnersServices from "./OwnersServices/OwnersServices";
+import OwnersTestimony from "./OwnersTestimony/OwnersTestimony";
+import OwnersProducts from "./OwnersProducts/OwnersProducts";
+import OwnerHeader from "./OwnersHeader/OwnersHeader";
 import { useEffect } from "react";
-import ContactCard from '../../components/contactCard/SadiqContactCard';
+import ContactCard from "../../components/contactCard/SadiqContactCard";
 
 const Owners = () => {
   useEffect(() => {
@@ -18,19 +18,18 @@ const Owners = () => {
   }, []);
   return (
     <>
-    <Navbar background="transparent" color="black" h3Color="black"/>
-    <OwnerHeader />
-    <div className="contact-card-div">
-    <ContactCard />
-    </div>
-    <OwnersBenefits />
-    <OwnersTestimony />
-    <OwnersServices />
-    <OwnersProducts />
-    <Footer />
-</>
+      <Navbar background="transparent" color="black" h3Color="black" />
+      <OwnerHeader />
+      <div className="contact-card-div">
+        <ContactCard />
+      </div>
+      <OwnersBenefits />
+      <OwnersTestimony />
+      <OwnersServices />
+      <OwnersProducts />
+      <Footer />
+    </>
   );
-}
+};
 
-export default Owners
-
+export default Owners;
